369a2f88479bdbc1711dfd8980c6dcb540975e00
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2024-03-15 11:44:56 -0300

    Add lesson 3

--- Turma 14/Projeto/src/main.ts
+++ Turma 14/Projeto/src/main.ts
@@ -1,75 +1,26 @@
-import crypto from "crypto";
-import pgp from "pg-promise";
+import AccountRepositoryDatabase from "./infra/repository/AccountRepositoryDatabase";
+import ExpressAdapter from "./infra/http/ExpressAdapter";
+import GetAccount from "./application/usecase/GetAccount";
+import LoggerConsole from "./infra/logger/LoggerConsole";
+import MainController from "./infra/controller/MainController";
+import PgPromiseAdapter from "./infra/database/PgPromiseAdapter";
+import Signup from "./application/usecase/Signup";
 
-export function validateCpf (cpf: string) {
-	if (!cpf) return false;
-	cpf = clean(cpf);
-	if (isInvalidLength(cpf)) return false;
-	if (allDigitsAreTheSame(cpf)) return false;
-	const dg1 = calculateDigit(cpf, 10);
-	const dg2 = calculateDigit(cpf, 11);
-	return extractCheckDigit(cpf) === `${dg1}${dg2}`;
-}
+// composition root ou entry point
+// criar o grafo de dependências utilizado no projeto
 
-function clean (cpf: string) {
-	return cpf.replace(/\D/g, "");
-}
+// framework and driver and library
+const httpServer = new ExpressAdapter();
+const databaseConnection = new PgPromiseAdapter();
 
-function isInvalidLength (cpf: string) {
-	return cpf.length !== 11;
-}
+// interface adapter
+const accountRepository = new AccountRepositoryDatabase(databaseConnection);
+const logger = new LoggerConsole();
 
-function allDigitsAreTheSame (cpf: string) {
-	return cpf.split("").every(c => c === cpf[0]);
-}
+// use case
+const signup = new Signup(accountRepository, logger);
+const getAccount = new GetAccount(accountRepository);
 
-function calculateDigit (cpf: string, factor: number) {
-	let total = 0;
-	for (const digit of cpf) {
-		if (factor > 1) total += parseInt(digit) * factor--;
-	}
-	const rest = total%11;
-	return (rest < 2) ? 0 : 11 - rest;
-}
-
-function extractCheckDigit (cpf: string) {
-	return cpf.slice(9);
-}
-
-export async function signup (input: any): Promise<any> {
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const accountId = crypto.randomUUID();
-		const [account] = await connection.query("select * from cccat14.account where email = $1", [input.email]);
-		if (account) throw new Error("Duplicated account");
-		if (isInvalidName(input.name)) throw new Error("Invalid name");
-		if (isInvalidEmail(input.email)) throw new Error("Invalid email");
-		if (!validateCpf(input.cpf)) throw new Error("Invalid cpf");
-		if (input.isDriver && isInvalidCarPlate(input.carPlate)) throw new Error("Invalid car plate");
-		await connection.query("insert into cccat14.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)", [accountId, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver]);
-		return {
-			accountId
-		};
-	} finally {
-		await connection.$pool.end();
-	}
-}
-
-function isInvalidName (name: string) {
-	return !name.match(/[a-zA-Z] [a-zA-Z]+/);
-}
-
-function isInvalidEmail (email: string) {
-	return !email.match(/^(.+)@(.+)$/);
-}
-
-function isInvalidCarPlate (carPlate: string) {
-	return !carPlate.match(/[A-Z]{3}[0-9]{4}/);
-}
-
-export async function getAccount (accountId: string) {
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	const [account] = await connection.query("select * from cccat14.account where account_id = $1", [accountId]);
-	await connection.$pool.end();
-	return account;
-}
+// interface adapter
+new MainController(httpServer, signup, getAccount);
+httpServer.listen(3000);
